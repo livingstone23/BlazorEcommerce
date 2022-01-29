c7af35e4e32785974dd1ca68599ecdf3ef8a2523
livingstone23 <livingstone23@gmail.com>
2022-01-29 18:09:57 +0100

    Sync with Github

--- BlazorEcommerce/Server/Program.cs
+++ BlazorEcommerce/Server/Program.cs
@@ -1,14 +1,27 @@
 global using BlazorEcommerce.Shared.Models;
+global using Microsoft.EntityFrameworkCore;
+global using BlazorEcommerce.Server.Data;
 using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
+//(1.1)Permite configurar Swagger una vez instalado el paquete Swashbuckle.AspNetCore
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+//(1.2)Permite configurar Swagger una vez instalado el paquete Swashbuckle.AspNetCore
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -21,6 +34,9 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+//(1.3)Permite configurar Swagger una vez instalado el paquete Swashbuckle.AspNetCore
+app.UseSwagger();
 
 app.UseHttpsRedirection();
 
--- BlazorEcommerce/Shared/Models/Product.cs
+++ BlazorEcommerce/Shared/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +15,9 @@ namespace BlazorEcommerce.Shared.Models
         public string Description { get; set; } = string.Empty;
         public string ImageUrl { get; set; } = string.Empty;
 
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
+
 
         //public Category? Category { get; set; }
         //public int CategoryId { get; set; }
